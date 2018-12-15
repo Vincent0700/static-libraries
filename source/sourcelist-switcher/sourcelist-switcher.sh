d9fafffa44700dbd56289f33d850dd355e05a890
vincent <wang.yuanqiu007@gmail.com>
2018-12-15 10:10:12 +0800

    :sparkles: add support for sourcelist-switcher
    new support for ubuntu 18.10

--- source/sourcelist-switcher/sourcelist-switcher.sh
+++ source/sourcelist-switcher/sourcelist-switcher.sh
@@ -7,48 +7,42 @@ echo -e "\e[36m*  @github: https://github.com/vincent0700      *\033[0m"
 echo -e "\e[36m*************************************************\033[0m"
 
 # Step 1: Collecting system version.
-echo -e "\r\n\e[36mStep 1. Collecting your system infomation.\033[0m"
-codename=$(lsb_release -a | grep -v LSB | grep Codename | awk '{print $2}')
-if [ $codename = "xenial" ]
-then
-  echo -e "Found your system's codename is \033[35m$codename\033[0m"
-  case $codename in 
-    xenial)
-      ;;
-    *)
-      echo "\033[31m[Error] Codename not supported.\033[0m"
-      exit 1
-      ;;
-  esac
-fi
+echo -e "\r\n\e[36mStep 1. Select your system version.\033[0m"
+versions=("xenial" "boinc" )
+paths=("ubuntu/xenial" "ubuntu/boinc")
+
+echo -e "\033[32m[1]\033[0m ubuntu 16.04 xenial (default)"
+echo -e "\033[32m[2]\033[0m ubuntu 18.04 boinc"
 
 Q1() {
-  echo -e "Is that right? (y/n)[y]: \c"
+  echo -e "Which version is your system? (1/2)[1]: \c"
   read ans
   case $ans in
-    y|"")
-      echo "Set codename to $codename"
+    1|"")
+      ans=1
+      echo "You choose ${versions[$ans-1]}"
       ;;
-    n)
-      echo "Thank you for using."
-      exit 1
+    2)
+      echo "You choose ${versions[$ans-1]}"
       ;;
     *)
       Q1
       ;;
   esac
+  version=${versions[$ans-1]}
+  path=${paths[$ans-1]}
 }
 Q1
 
 # Step 2: Choosing the mirror you want to use.
 echo -e "\r\n\e[36mStep 2. Select the mirror you want to use.\033[0m"
-mirrors=("aliyun" "official" "163" "tsinghua")
-echo $mirrors
+mirrors=("aliyun" "official" "163" "tsinghua" "ustc")
 
 echo -e "\033[32m[1]\033[0m aliyun (default)"
 echo -e "\033[32m[2]\033[0m official (us mirror)"
 echo -e "\033[32m[3]\033[0m 163"
 echo -e "\033[32m[4]\033[0m tsinghua"
+echo -e "\033[32m[4]\033[0m ustc"
 
 Q2() {
   echo -e "Which mirror you want to use? (1/2/3/4)[1]: \c"
@@ -56,27 +50,27 @@ Q2() {
   case $ans in
     1|"")
       ans=1
-      echo "Setting mirror to ${mirrors[$ans-1]}"
+      echo "You choose ${mirrors[$ans-1]}"
       ;;
-    2|3|4)
-      echo "Setting mirror to ${mirrors[$ans-1]}"
+    2|3|4|5)
+      echo "You choose ${mirrors[$ans-1]}"
       ;;
     *)
       Q2
       ;;
   esac
   mirror=${mirrors[$ans-1]}
-  url="https://raw.githubusercontent.com/Vincent0700/static-libraries/master/source/sourcelist-switcher/sources/ubuntu/xenial/$mirror.txt"
-  echo -e "Downding sourcelist ..."
+  url="https://raw.githubusercontent.com/Vincent0700/static-libraries/master/source/sourcelist-switcher/sources/$path/$mirror.txt"
+  echo -e "Downding $url"
 
   file="/etc/apt/sources.list"
-  sudo wget -q -O "$file.tmp" $url;
+  wget -q -O "$file.tmp" $url;
   if [ ! -f "$file.tmp" ]; then
     echo -e "\033[31m[Error] File not found.\033[0m"
     exit 1
   fi
-  sudo mv "$file" "$file.backup";
-  sudo mv "$file.tmp" "$file";
+  mv "$file" "$file.backup";
+  mv "$file.tmp" "$file";
 
   echo -e "Successfully replace /etc/apt/sources.list"
   echo -e "The old version has been moved to /etc/apt/sources.list.back"
@@ -85,4 +79,4 @@ Q2
 
 # Step 3: Execute apt update.
 echo -e "\r\n\e[36mStep 3. Execute apt update."
-sudo apt update
+apt update
