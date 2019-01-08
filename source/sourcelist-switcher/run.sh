41cfe3612de6683df9a542cbd792a7fd32ccda45
vincent <wang.yuanqiu007@gmail.com>
2019-01-08 19:26:00 +0800

    refactor: relactor sourcelist-switcher

---
+++ source/sourcelist-switcher/run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+isDev=true
+
+basePath=$(cd `dirname $0`; pwd)
+reposUrl="https://raw.githubusercontent.com/Vincent0700/static-libraries/master/source/sourcelist-switcher"
+
+libs=(
+    "libs/inquirer/list-input.sh"
+)
+
+os_versions=(
+    "ubuntu_14.04_trusty"
+    "ubuntu_16.04_xenial"
+    "ubuntu_18.04_bionic"
+)
+paths=(
+    "sources/ubuntu/trusty" 
+    "sources/ubuntu/xenial" 
+    "sources/ubuntu/bionic"
+)
+mirrors=(
+    "aliyun" 
+    "official" 
+    "163" 
+    "tsinghua" 
+    "ustc"
+)
+
+for libpath in ${libs[@]}; do
+    if [ "$isDev" = true ]; then
+        source $basePath/$libpath
+    else
+        source "$(wget -q -O - $reposUrl/$libpath)"
+    fi
+done
+
+function IndexOf() {
+    params=($@)
+    element=$1
+    arr=($@)
+    unset arr[0]
+    arr=("${arr[@]}")
+
+    for i in "${!arr[@]}"; do 
+        item=${arr[$i]}
+        if [ "$item" = "$element" ]; then
+            echo $i
+            return
+        fi
+    done
+    echo -1
+}
+
+echo "[Sourcelist Switcher] version 1.0.1"
+echo "repos: https://github.com/Vincent0700/static-libraries/tree/master/source/sourcelist-switcher"
+echo ""
+
+list_input "Choose your OS version." os_versions selected
+selected_os_version=$(IndexOf "$selected" "${os_versions[@]}")
+
+list_input "Choose your OS version." mirrors selected
+selected_mirror=$(IndexOf "$selected" "${mirrors[@]}")
+
+url="$reposUrl/${paths[$selected_os_version]}/${mirrors[$selected_mirror]}.txt"
+file="/etc/apt/sources.list"
+wget -q -O "$file.tmp" $url;
+if [ ! -f "$file.tmp" ]; then
+echo -e "\033[31m[Error] File not found.\033[0m"
+exit 1
+fi
+
+apt update
