b5d819f97fca0af6546c12e7eaf88d2adb53027f
vincent <wang.yuanqiu007@gmail.com>
2019-02-15 18:14:43 +0800

    fix: path problem

--- source/zsh-installer/run.sh
+++ source/zsh-installer/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-$ZSH_PATH="$HOME/.oh-my-zsh/custom"
-
 sudo apt-get install -y git zsh wget curl
 chsh -s /bin/zsh
 
@@ -9,10 +7,10 @@ chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install Plugin: zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PATH/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/zsh-syntax-highlighting
 
 # Install Plugin: zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_PATH/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/zsh-autosuggestions
 
 # Replace profile
 wget https://raw.githubusercontent.com/Vincent0700/static-libraries/master/source/zsh-installer/.zshrc -O ~/.zshrc
